2a056fe92e01932a011016fb9f86d0355ab18fcc
RONAK BHATT <ronakabhattrz@gmail.com>
2021-09-23 00:14:52 +0530

    add gogole tag manager package

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,6 +5,14 @@ import Footer from './Footer';
 import Nav from './Nav';
 
 import styles from '@styles/Home.module.css';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-PTQB8D3',
+  preview: 'env-2',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 type LayoutProps = {
   children?: React.ReactNode;
@@ -25,11 +33,16 @@ const Layout: React.FC<LayoutProps> = ({ title = 'Ronak Bhatt', children }) => (
         content="I'm a Full Stack software engineer with solid experience with Ruby on Rails and Javascript Frameworks. I've worked in applications in several business areas and with several technologies."
       />
       <meta
+        name="description"
+        content="I'm a Full Stack software engineer with solid experience with Ruby on Rails and Javascript Frameworks. I've worked in applications in several business areas and with several technologies."
+      />
+      <meta
         name="keywords"
         content="Ronak, Ronak Bhatt, Ruby, Ruby on Rails, MySQL, Postgres, react js, Typescript, next js, Heroku, AWS, ronakbhattrz, ronakabhattrz, ronak songs, ronak bhatt ruby on rails, ruby on rails developer, react js developer"
       />
       <meta name="robots" content="index, follow" />
       <meta name="language" content="English" />
+      <meta http-equiv="content-language" content="en" />
       <meta name="revisit-after" content="1 days" />
       <meta name="author" content="Ronak Bhatt" />
       <meta property="og:type" content="website" />
